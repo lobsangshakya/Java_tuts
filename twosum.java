70c3359c56064bc2a435c86cde451b3c1af92d35
Lobsang Tsetan Shakya <lobsangshakya5@gmail.com>
2025-08-29 14:28:49 +0530

    Update

--- twosum.java
+++ twosum.java
@@ -14,14 +14,14 @@ class twosum {
     }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter size of array: ");
+        System.out.print("Enter size of array: ");
         int size_arr = scanner.nextInt();
         int[] nums = new int[size_arr];
         System.out.print("Enter" +size_arr+ "arrays: ");
         for (int i = 0; i<size_arr; i++) {
             nums[i] = scanner.nextInt();
         }
-        System.out.println("Enter target: ");
+        System.out.print("Enter target: ");
         int target = scanner.nextInt();
         twosum obj = new twosum();
         int[] result = obj.twoSum(nums, target);
